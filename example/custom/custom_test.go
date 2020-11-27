4341268f67e06a12a311a08f4f1759a200f25b24
Dmitry Dzhumagaliev <judimator@gmail.com>
2020-11-27 15:47:04 +0300

    Added some more tests.

--- example/custom/custom_test.go
+++ example/custom/custom_test.go
@@ -12,26 +12,45 @@ func TestAssignmentAsExpression(t *testing.T) {
 	test.AddFile(`<?php
 	// phpdoc annotations are not required for NoVerify in simple cases
 	function something() {
-		$a = "test";
+		$a = 'test';
 		return $a;
 	}
 	function in_array() {}
 
 	function test() {
-		$b = ["1", "2", "3"];
+		$b = ['1', '2', '3'];
 
 		if (in_array(something(), $b)) {
-			echo "third arg true";
+			echo 'third arg true';
 		}
 
 		if (something() == $b[1]) {
-			echo "must be ===";
+			echo 'must be ===';
 		}
 	}`)
 
 	test.Expect = []string{
 		"3rd argument of in_array must be true when comparing strings",
 		"Strings must be compared using '===' operator",
+	}
+
+	test.RunAndMatch()
+}
+
+func TestSingeQuotedString(t *testing.T) {
+	test := linttest.NewSuite(t)
+
+	test.AddFile(`<?php
+$var0 = "string".'other string';
+$var1 = "string";
+$var2 = "other $var1";
+$var3 = 'another';
+$var4 = "another {$var3}";
+`)
+
+	test.Expect = []string{
+		"Strings must be declared using ''",
+		"Strings must be declared using ''",
 	}
 
 	test.RunAndMatch()
